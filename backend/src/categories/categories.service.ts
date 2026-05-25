7ef94ac62ce08d397a3040c8fd67056835f172ab
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-05-25 18:52:12 +0300

    git commit -m "feat: implement categories findAll and cleanup unused dto and methods"

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,26 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCategoryDto } from './dto/create-category.dto';
-import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all categories`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
-
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async findAll() {
+    return await this.prisma.category.findMany();
   }
 }
